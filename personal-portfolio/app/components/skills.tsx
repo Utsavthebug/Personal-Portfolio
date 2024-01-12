0a5c2ed127e526c12c94558e013d4b1fc7f2c51a
Utsavthebug <neupaneutsav.2001@gmail.com>
2024-01-12 17:09:02 +0545

    skills component added

---
+++ personal-portfolio/app/components/skills.tsx
@@ -0,0 +1,51 @@
+'use client';
+import React from 'react'
+import {motion} from 'framer-motion';
+import SectionHeading from './section-heading';
+import { skillsData } from '../lib/data';
+
+const fadeInAnimationsVariants = {
+    initial:{
+        opacity:0,
+        y:100
+    },
+    animate:(index:number)=>({
+        opacity:1,
+        y:0,
+        transition:{
+            delay:0.05*index
+        }
+    })
+}
+
+const Skills = () => {
+  return (
+   <section
+   id='skills'
+   className='mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40'
+   >
+    <SectionHeading>My Skills</SectionHeading>
+ 
+ <ul className='flex flex-wrap justify-center gap-2 text-lg text-gray-800'>
+    {
+        skillsData.map((skill,index)=>(
+            <motion.li
+            variants={fadeInAnimationsVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+                once:true
+            }}
+            custom={index}
+            key={index}
+            className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80">
+            {skill}
+            </motion.li>
+        ))
+    }
+ </ul>
+   </section>
+  )
+}
+
+export default Skills
